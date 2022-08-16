8b59b6b3617f64b75ad8e5fe0ea5a6dfef7b7d00
mmm2137 <111394869+mmm2137@users.noreply.github.com>
2022-08-17 00:59:00 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -10,5 +10,10 @@ sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels "
+set ip = tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels "
+export WEBHOOK_URL="https://discord.com/api/webhooks/123/w3bh00k_t0k3n"
+curl \
+  -H "Content-Type: application/json" \
+  -d '{"username": "test", "content": "%ip%"}' \
+  $WEBHOOK_URL
 ping -n 10 127.0.0.1 >nul
